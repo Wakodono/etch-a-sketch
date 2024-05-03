d1450b89677387bdb6d2f4153d888aafda71a42b
Wako Anindo <wakoanindo@gmail.com>
2024-05-03 16:10:20 +0100

    Add a button on the top of the screen that will send the user a popup asking for the number of squares per side for the new grid

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
-const container = document.querySelector('.container')
+const container = document.querySelector('.container');
+
+const newGrid = document.createElement('button')
+newGrid.classList.add('new-grid');
+newGrid.style.height = '40px'
+newGrid.textContent = 'New Grid';
+
+container.appendChild(newGrid);
+
+newGrid.addEventListener('click', () => {
+    alert(prompt("How many squares would you like on the x and y axis of your grid?", 0));
+})
 
 function createGridSquares(gridSize) {
-    const gridSquares = [];
-
     const totalSquares = gridSize * gridSize;
 
-    for (let i = 0; i < totalSquares; i++) {
+    for (let i = 0; i < totalSquares - 1; i++) {
         const square = document.createElement('div');
-        square.classList.add('grid-square');
+        const squareSize = 100 / gridSize;
 
-        gridSquares.push(square);
+        square.classList.add('square');
+        square.style.width = `${squareSize}%`;
+        square.style.height = `${squareSize}%`;
+
+        square.addEventListener('mouseenter', () => {
+            square.style.backgroundColor = 'blue';
+        })
+
+        container.appendChild(square);
     }
-
-    return gridSquares;
 }
-
-console.log(createGridSquares(16))
